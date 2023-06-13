0830561f642b86e5f5d72d0cbb37d8e6a2a7a477
Dogukan <dbabuyukarslan@gmail.com>
2023-06-13 22:39:57 +0300

    read write added

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -6,21 +6,85 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace FinalProject
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void ReadFromTable(SqlConnection connection)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                // Veri çekme iþlemi
+                string selectQuery = "SELECT * FROM Table_1";
+                SqlCommand selectCommand = new SqlCommand(selectQuery, connection);
+
+                SqlDataReader reader = selectCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    string column1Value = reader.GetString(0);
+                    int column2Value = reader.GetInt32(1);
+                    // ... Diðer sütun deðerlerini alabilirsiniz
+
+                    Console.WriteLine($"Blood Type: {column1Value}, Donor Name: {column2Value}");
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Veri çekme hatasý: " + ex.Message);
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        static void WriteToTable(SqlConnection connection)
+        {
+            try
+            {
+                // Veri yazma iþlemi
+                string insertQuery = "INSERT INTO Table_1 ([Blood Type], [Donor Name], [Donation Date], [Units]) VALUES (@value1, @value2, @value3, @value4)";
+                SqlCommand insertCommand = new SqlCommand(insertQuery, connection);
+
+                DateTime dateTime = DateTime.Now;
+
+                // Parametre deðerlerini belirleme
+                insertCommand.Parameters.AddWithValue("@value1", "Test1");
+                insertCommand.Parameters.AddWithValue("@value2", "Test2");
+                insertCommand.Parameters.AddWithValue("@value3", dateTime);
+                insertCommand.Parameters.AddWithValue("@value4", 2);
+
+                int rowsAffected = insertCommand.ExecuteNonQuery();
+
+                Console.WriteLine($"Etkilenen satýr sayýsý: {rowsAffected}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Veri yazma hatasý: " + ex.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=tcp:final-project-yu.database.windows.net,1433;Initial Catalog=blood;Persist Security Info=False;User ID=TheViperI2KI;Password=Gs605dba!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    connection.Open();
+                    Console.WriteLine("Baðlantý baþarýlý!");
+
+                    // Tablodan veri okuma
+                    ReadFromTable(connection);
+
+                    // Tabloya veri ekleme
+                    WriteToTable(connection);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Baðlantý veya iþlem hatasý: " + ex.Message);
+                }
+            }
+        }
     }
 }
